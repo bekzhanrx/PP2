f8edcf31d8d1e4c4ebedbed71347cf490af9523a
bekzhanrx <123236795+bekzhanrx@users.noreply.github.com>
2023-01-22 00:36:51 +0600

    Add files via upload

---
+++ 30.py
@@ -0,0 +1,19 @@
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+b = "Hello, World!"
+print(len(b))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+print("expensive" not in txt)
+
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
