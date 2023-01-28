3c39324e84c60ea6a064522c5862403975ab8deb
bekzhanrx <123236795+bekzhanrx@users.noreply.github.com>
2023-01-29 00:39:32 +0600

    Add files via upload

---
+++ Lab2/24.py
@@ -0,0 +1,13 @@
+print(6 & 3)
+
+
+
+"""
+The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+2 = 0000000000000010
+====================
+"""
---
+++ Lab2/25.py
@@ -0,0 +1,13 @@
+print(6 | 3)
+
+
+
+"""
+The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+7 = 0000000000000111
+====================
+"""
