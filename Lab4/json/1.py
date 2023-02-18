c010ec70b88a1c47458fe72e8894db3beb9cdc2c
bekzhanrx <123236795+bekzhanrx@users.noreply.github.com>
2023-02-18 15:46:52 +0600

    Add files via upload

---
+++ Lab4/json/1.py
@@ -0,0 +1,16 @@
+import json
+
+with open("sample-data.json", 'r') as file:
+    data = json.load(file)
+
+print("Interface Status")
+print("================================================================================")
+print("DN                                                 Description           Speed    MTU")
+print("-------------------------------------------------- --------------------  ------  ------")
+cnt = 0
+for item in data["imdata"]:
+    if cnt == 3:
+        break
+    print(item["l1PhysIf"]["attributes"]["dn"], "                            ",
+          item["l1PhysIf"]["attributes"]["speed"], " ", item["l1PhysIf"]["attributes"]["mtu"])
+    cnt += 1
